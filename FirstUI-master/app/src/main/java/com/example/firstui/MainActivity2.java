b83510273e9ca7d71dfaf7ddd8a400427b9e1763
Yu YANG <yuy6@illinois.edu>
2019-12-02 17:03:59 -0600

    cahnge sth

---
+++ FirstUI-master/app/src/main/java/com/example/firstui/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.firstui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+        Button button = findViewById(R.id.searchButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+    }
+}
