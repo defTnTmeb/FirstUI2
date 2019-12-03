16256f58e280dc11d1f96f69a7957c036e1a57d0
Yu YANG <yuy6@illinois.edu>
2019-12-03 09:41:50 -0600

    ji

--- FirstUI-master/app/src/main/java/com/example/firstui/MainActivity2.java
+++ FirstUI-master/app/src/main/java/com/example/firstui/MainActivity2.java
@@ -4,22 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity2 extends AppCompatActivity {
+    //public static ArrayList<String> responseFromApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second);
-        Button button = findViewById(R.id.searchButton);
+        //setContentView(R.layout.activity_main2);
+        //TextView textView = findViewById(R.id.textView);
+        //textView.setText("");
+        //for (int i = 0; i < responseFromApi.size(); i++) {
+        //    textView.append(responseFromApi.get(i));
+        //    textView.append("\n");
+        //}
+        Button button = findViewById(R.id.returnButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity2.this, MainActivity.class);
                 startActivity(intent);
-                //finish();
+                finish();
             }
         });
     }
